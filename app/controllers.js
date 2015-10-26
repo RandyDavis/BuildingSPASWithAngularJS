4e1223413054d41eb11152d46843edd1cbbe20a6
Randy Davis <randy_davis@outlook.com>
2015-10-25 22:26:11 -0500

    complete Building a Todo App pt1

--- app/controllers.js
+++ app/controllers.js
@@ -2,5 +2,20 @@
 
 angular.module('todoApp', [])
     .controller('todoController', ["$scope", function ($scope) {
-        $scope.newTask = "vacuum floor";
-    }])
+        $scope.newTask = "";
+
+        $scope.taskList = [
+            {description: "Buy airplane tickets", done: false},
+            {description: "Make hotel reservations", done: false},
+            {description: "Chill Like A Boss", done: false}
+        ];
+
+        $scope.addTodo = function () {
+            $scope.taskList.push({description: $scope.newTask, done:false});
+            $scope.newTask = "";
+        };
+
+        $scope.deleteTodo = function (index) {
+            $scope.taskList.splice(index, 1);
+        };
+    }]);
