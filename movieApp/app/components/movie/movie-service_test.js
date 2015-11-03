250ae151ebb100642e178e9bbfefa1f2450a5bb5
Randy Davis <randy_davis@outlook.com>
2015-11-02 23:40:52 -0600

    add example paginate-filter unit test; add file or movie-service_test for upcoming movie test;

---
+++ movieApp/app/components/movie/movie-service_test.js
@@ -0,0 +1,3 @@
+/**
+ * Created by randy on 11/2/15.
+ */
--- movieApp/karma.conf.js
+++ movieApp/karma.conf.js
@@ -1,14 +1,17 @@
 module.exports = function(config){
   config.set({
 
-    basePath : 'angular-seed',
+    basePath : './',
 
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.japps',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/catalog/*.js',
+      'app/genre/*.js',
+      'app/movie/*.js'
     ],
 
     autoWatch : true,
